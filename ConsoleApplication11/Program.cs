de6699a344231bc50c9a6d2cd853dc4af1ebcbee
Erick Stubbs <es@hotpressweb.com>
2011-11-08 09:35:37 -0700

    initial commit

---
+++ ConsoleApplication11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication11
+{
+   class Program
+   {
+      static void Main(string[] args) {
+         rec record = new rec { id = 1, mydecimal = null };
+         List<rec> records = new List<rec> { record };
+
+         FileHelpers.FileHelperEngine<rec> engine = new FileHelpers.FileHelperEngine<rec>();
+
+         Console.WriteLine(engine.WriteString(records));
+
+      }
+   }
+
+   [FileHelpers.DelimitedRecord(",")]
+   public class rec
+   {
+      public int id;
+      public decimal? mydecimal;
+
+   }
+}
